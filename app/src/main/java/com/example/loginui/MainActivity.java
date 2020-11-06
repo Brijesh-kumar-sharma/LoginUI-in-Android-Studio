ac1cd637b866c49c6087a1179220b0d4c3052d60
Brijesh Kumar Sharma <brijeshkumarsharma0002@gmail.com>
2020-11-05 17:54:19 -0800

    Second Commit

--- app/src/main/java/com/example/loginui/MainActivity.java
+++ app/src/main/java/com/example/loginui/MainActivity.java
@@ -3,6 +3,13 @@ package com.example.loginui;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,45 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button create = findViewById(R.id.newAccountButton);
+        create.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"You clicked Create an account",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        TextView forget = findViewById(R.id.forget);
+        forget.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this,"You clicked Forget Password",Toast.LENGTH_LONG).show();
+            }
+        });
+
+        final EditText username = findViewById(R.id.username_edit);
+        final EditText password = findViewById(R.id.password_edit);
+
+        RelativeLayout signin = findViewById(R.id.signinButton);
+        signin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(username.getText().toString())) {
+
+                    if (!TextUtils.isEmpty(password.getText().toString())) {
+
+                        Toast.makeText(MainActivity.this,"Account is created",Toast.LENGTH_LONG).show();
+
+                    }
+                }
+                if (TextUtils.isEmpty(username.getText().toString())) {
+                    Toast.makeText(MainActivity.this,"Username can't left empty",Toast.LENGTH_LONG).show();
+                }
+
+                if (TextUtils.isEmpty(password.getText().toString())) {
+                    Toast.makeText(MainActivity.this,"Password can't left empty",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
